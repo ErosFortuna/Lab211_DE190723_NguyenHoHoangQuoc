4cde864317315c8737b1ba980ee2e81134cb5c7c
ErosFortuna <hoangquoc316@gmail.com>
2025-01-10 18:00:09 +0700

    Add files via upload

--- LAB211_NguyenHoHoangQuoc/src/main/java/week1/LAB211_NguyenHoHoangQuoc_week1.java
+++ LAB211_NguyenHoHoangQuoc/src/main/java/week1/LAB211_NguyenHoHoangQuoc_week1.java
@@ -16,11 +16,12 @@ public class LAB211_NguyenHoHoangQuoc_week1 {
         Scanner scanner = new Scanner(System.in);
         int option = 0;
         while (option != 5) {
-            System.out.println("=======================================");
-            System.out.println("menu:");
+            System.out.printf("\n=======================================");
+            System.out.println("\nmenu:");
             System.out.println("1.Letter and character count");
             System.out.println("2.Linear search");
             System.out.println("3.Change base number system (16, 10, 2) program");
+            System.out.println("4.Solving the equation, find the square numbers, even numbers, and odd numbers.");
             System.out.println("5.exit");
             System.out.println("=======================================");
             option = Integer.parseInt(scanner.nextLine());
@@ -67,7 +68,132 @@ public class LAB211_NguyenHoHoangQuoc_week1 {
                     }
                     System.out.println("=======================================");
                 }
-
+                case 4: {
+                    System.out.println("=======================================");
+                    System.out.println("========= Equation Program =========\n"
+                            + "1. Calculate Superlative Equation\n"
+                            + "2. Calculate Quadratic Equation\n"
+                            + "3. Exit\n"
+                            + "Please choice one option:");
+                    int choice = Integer.parseInt(scanner.nextLine());
+                    EquationSolving check = new EquationSolving();
+                    switch (choice) {
+                        case 1: {
+                            System.out.println("Enter A:");
+                            String temp;
+                            double a = Double.NaN;
+                            while (Double.isNaN(a)) {
+                                temp = scanner.nextLine();
+                                a = check.checkValidNumber(temp);
+                            }
+                            System.out.println("Enter B:");
+                            double b = Double.NaN;
+                            while (Double.isNaN(b)) {
+                                temp = scanner.nextLine();
+                                b = check.checkValidNumber(temp);
+                            }
+                            EquationSolving equation = new EquationSolving(a, b, 0);
+                            System.out.println("solution: x = " + equation.calculateEquation());
+                            if (check.isOdd(a) && check.isOdd(b)) {
+                                System.out.println("Number is Odd: " + a + "," + b);
+                            } else if (!check.isOdd(a) && !check.isOdd(b)) {
+                                System.out.println("Number is even: " + a + "," + b);
+                            } else {
+                                System.out.print("Number is Odd: ");
+                                if (check.isOdd(a)) {
+                                    System.out.print(a + ",");
+                                }
+                                if (check.isOdd(b)) {
+                                    System.out.print(b);
+                                }
+                                System.out.printf("\nNumber is even: ");
+                                if (!check.isOdd(a)) {
+                                    System.out.print(a + ",");
+                                }
+                                if (!check.isOdd(b)) {
+                                    System.out.print(b);
+                                }
+                            }
+                            System.out.print("\nNumber is Perfect Square: ");
+                            if (check.isPerfectSquare(a)) {
+                                System.out.print(a + ",");
+                            }
+                            if (check.isPerfectSquare(b)) {
+                                System.out.print(b);
+                            }
+                        }
+                        case 2: {
+                            System.out.println("Enter A:");
+                            String temp;
+                            double a = Double.NaN;
+                            while (Double.isNaN(a)) {
+                                temp = scanner.nextLine();
+                                a = check.checkValidNumber(temp);
+                            }
+                            System.out.println("Enter B:");
+                            double b = Double.NaN;
+                            while (Double.isNaN(b)) {
+                                temp = scanner.nextLine();
+                                b = check.checkValidNumber(temp);
+                            }
+                            System.out.println("Enter C:");
+                            double c = Double.NaN;
+                            while (Double.isNaN(c)) {
+                                temp = scanner.nextLine();
+                                c = check.checkValidNumber(temp);
+                            }
+                            EquationSolving equation = new EquationSolving(a, b, c);
+                            int i=0;
+                            if(equation.calculateQuadraticEquation()==null){
+                                System.out.println("no solution");
+                            }else if(equation.calculateQuadraticEquation().isEmpty()){
+                                System.out.println("infinitely many solutions");
+                            }else{
+                                System.out.print("solution: ");
+                                for(double x:equation.calculateQuadraticEquation()){
+                                    System.out.print("x"+(i+1)+" = "+x+",");
+                                    i++;
+                                }
+                            }
+                            if (check.isOdd(a) && check.isOdd(b)&&check.isOdd(c)) {
+                                System.out.println("\nNumber is Odd: " + a + "," + b+","+c);
+                            } else if (!check.isOdd(a) && !check.isOdd(b)&&!check.isOdd(c)) {
+                                System.out.println("\nNumber is even: " + a + "," + b+","+c);
+                            } else {
+                                System.out.print("\nNumber is Odd: ");
+                                if (check.isOdd(a)) {
+                                    System.out.print(a + ",");
+                                }
+                                if (check.isOdd(b)) {
+                                    System.out.print(b+ ",");
+                                }
+                                if (check.isOdd(c)) {
+                                    System.out.print(c);
+                                }
+                                System.out.printf("\nNumber is even: ");
+                                if (!check.isOdd(a)) {
+                                    System.out.print(a + ",");
+                                }
+                                if (!check.isOdd(b)) {
+                                    System.out.print(b+ ",");
+                                }
+                                if (!check.isOdd(c)) {
+                                    System.out.print(c);
+                                }     
+                            }
+                            System.out.print("\nNumber is Perfect Square: ");
+                            if (check.isPerfectSquare(a)) {
+                                System.out.print(a + ",");
+                            }
+                            if (check.isPerfectSquare(b)) {
+                                System.out.print(b+ ",");
+                            }
+                            if (check.isPerfectSquare(c)) {
+                                System.out.print(c);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
